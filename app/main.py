1cef362fb46548512e197dd003e25f9c2acae0d9
Герман Гриценко <gerfest.gricenko@gmail.com>
2024-03-26 16:52:55 +0500

    feat: Add logging for the listener mode

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import asyncio
 
+from loguru import logger
+
 from app.config import settings
 from app.listener import SimpleBusinessLogic
 
 
 async def main():
+    logger.info(f'Listener started with mode - {settings.MODE}')
     service = SimpleBusinessLogic(settings.MODE)
     await service.start()
 
